63c7423558be1359cb41216d56cdd08ee48928dd
Patel Vaishakhi <patel.vaishakhi@corp.udaan.com>
2020-11-04 18:04:10 +0530

    'hostupdate'

--- report/settings.py
+++ report/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '46_gw!=oz$!y%pvx%cx$k&7h0i60!4gdo$@j4iotc!4rqneh7j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.25.26.121','127.0.0.1']
+ALLOWED_HOSTS = ['172.25.26.121','127.0.0.1','172.31.20.49']
 
 
 
